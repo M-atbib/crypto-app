41e17062f75cd17160bd97356936dcd3d85baea1
Mokhtar Atbib <94636437+M-atbib@users.noreply.github.com>
2023-10-23 11:17:13 +0100

    addedrepo

---
+++ src/components/Exchanges.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Exchanges = () => {
+  return (
+    <div>
+      <h3>Exchanges</h3>
+      <p> Note: To access this endpoint you need premium plan</p>
+    </div>
+  );
+};
+
+export default Exchanges;
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,33 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+  "X-RapidAPI-Key": "ff0af6fb76msh71d87af76de12b5p18cd13jsn9ac3f9ef2199",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+    getCryptoDetails: builder.query({
+      query: (coinId) => createRequest(`/coin/${coinId}`),
+    }),
+    getCryptoHistory: builder.query({
+      query: (coinId, timeperiod) =>
+        createRequest(`/coin/${coinId}/history?timePeriod=${timeperiod}`),
+    }),
+  }),
+});
+
+export const {
+  useGetCryptosQuery,
+  useGetCryptoDetailsQuery,
+  useGetCryptoHistoryQuery,
+} = cryptoApi;
